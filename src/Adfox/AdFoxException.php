49f54cae400bf6361283003bc32d4fcb9cc63163
vleukhin <vleukhin@relevantmedia.ru>
2016-07-04 12:25:13 +0300

    Call Adfox API method

---
+++ src/Adfox/AdFoxException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AdFox;
+
+use \Exception;
+
+class AdfoxException extends Exception{
+
+}
--- src/Adfox/Adfox.php
+++ src/Adfox/Adfox.php
@@ -12,7 +12,7 @@ class AdFox {
 	 *
 	 * @var string
 	 */
-	protected $apiUrl = 'https://login.adfox.ru/API.php';
+	protected $apiUrl = 'https://api.adfox.ru/v1/API.php';
 
 	/**
 	 * User login.
@@ -28,6 +28,10 @@ class AdFox {
 	 */
 	protected $password = null;
 
+	const CODE_NO_ERROR = 0;
+	const CODE_AUTH_ERROR = -1;
+	const CODE_API_CALL_ERROR = 60;
+
 	/**
 	 * AdFox constructor.
 	 *
@@ -37,7 +41,7 @@ class AdFox {
 	public function __construct($login, $password)
 	{
 		$this->login = $login;
-		$this->password = $password;
+		$this->password = hash('sha256', $password);
 	}
 
 	/**
@@ -51,5 +55,53 @@ class AdFox {
 		$this->apiUrl = $url;
 
 		return $this;
+	}
+
+	/**
+	 * Call Adfox API
+	 *
+	 * @param $object
+	 * @param $action
+	 * @param $actionObject
+	 * @param array $parameters
+	 * @return \SimpleXMLElement[]
+	 * @throws AdfoxException
+	 */
+	protected function callApi($object, $action, $actionObject, $parameters = [])
+	{
+		$request = [
+			'loginAccount' => $this->login,
+			'loginPassword' => $this->password,
+			'object' => $object,
+			'action' => $action,
+			'actionObject' => $actionObject,
+		];
+
+		$request += $parameters;
+
+		$curl = curl_init($this->apiUrl);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
+
+		$response = curl_exec($curl);
+
+		if ($response == false)
+		{
+			throw new AdfoxException(curl_error($curl), self::CODE_API_CALL_ERROR);
+		}
+
+		$response = new \SimpleXMLElement($response);
+
+		if (empty($response))
+		{
+			throw new AdfoxException('Empty AdFox response', self::CODE_API_CALL_ERROR);
+		}
+		elseif((string) $response->status->code != self::CODE_NO_ERROR)
+		{
+			throw new AdfoxException((string) $response->status->error, (int) $response->status->code);
+		}
+
+		return $response->result;
 	}
 }
