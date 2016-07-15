a0e44f280fd1a5a8960f1336b8e58515798527b6
vleukhin <vleukhin@relevantmedia.ru>
2016-07-15 17:07:22 +0300

    Now AdFoxException has request info

--- src/Adfox/AdFoxException.php
+++ src/Adfox/AdFoxException.php
@@ -6,4 +6,35 @@ use \Exception;
 
 class AdfoxException extends Exception{
 
+	/**
+	 * Request params caused this exception
+	 *
+	 * @var array
+	 */
+	protected $request;
+
+	/**
+	 * AdfoxException constructor.
+	 *
+	 * @param string $message
+	 * @param int $code
+	 * @param Exception $previous
+	 * @param array $request
+	 */
+	public function __construct($message = '', $code = 0, Exception $previous = null, $request = [])
+	{
+		$this->request = $request;
+
+		parent::__construct($message, $code, $previous);
+	}
+
+	/**
+	 * Get request params caused this exception
+	 *
+	 * @return array
+	 */
+	public function getRequest()
+	{
+		return $this->request;
+	}
 }
